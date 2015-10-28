33546c1bf56bf98986171b7dd319b541e4625887
Pavel Grudinkin <pgrudinkin@gmail.com>
2015-10-28 21:03:51 +0300

    Промежуточный коммит

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :restrict_registration, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,8 +40,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    user = User.find(params[:id])
+      unless current_user?(user)
+        user.destroy
+        flash[:success] = "User deleted."
+      end
     redirect_to users_url
   end
 
@@ -69,5 +73,9 @@ class UsersController < ApplicationController
       def correct_user
         @user = User.find(params[:id])
         redirect_to(root_url) unless current_user?(@user)
+      end
+
+      def restrict_registration
+        redirect_to root_url, notice: "You are already regsitered." if signed_in?
       end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    it { should_not have_link('Users') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+    it { should_not have_link('Sign out', href: signout_path) }
+    it { should have_link('Sign in', href: signin_path) }
   end
 
   describe "signin" do
@@ -29,9 +34,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        sign_in user
       end
 
       it { should have_title(user.name) }
@@ -74,15 +77,25 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -116,5 +129,17 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_path) }
       end
     end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+
+      describe "should not be able to delete themselves via #destroy action" do
+        specify do
+          expect { delete user_path(admin) }.not_to change(User, :count)
+        end
+      end
+    end
+
   end
 end
