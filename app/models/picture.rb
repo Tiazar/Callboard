c38656ff74b93babc894056744ca1889331f68e0
Pavel Grudinkin <pgrudinkin@gmail.com>
2015-12-19 01:47:30 +0300

    небольшие правки картинок

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,9 @@ class Picture < ActiveRecord::Base
   belongs_to :post
 
   has_attached_file :image,
-    :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    path: ":rails_root/public/images/:id/:style/:filename",
+    url: "/images/:id/:style/:filename"
 
   do_not_validate_attachment_file_type :image
 end
